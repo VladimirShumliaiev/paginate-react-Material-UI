b81a22c41ce0af665a0c52926a500a7d29068a92
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-06-07 11:38:36 +0300

    06.06.2023

--- my-app/src/Componets/HomePage.jsx
+++ my-app/src/Componets/HomePage.jsx
@@ -6,61 +6,61 @@ import {Link as NavLink} from 'react-router-dom'
 
 const BASE_URL = 'http://hn.algolia.com/api/v1/search?';
 
-const HomePage = (props) => {
-    console.log(props)
+const HomePage = () => {
     const [posts, setPosts] = useState([])
+    const [query, setQuery] = useState('query')
     const [page, setPage] = useState(1)
     const [pageQty, setPageQty] = useState(0)
-    const [query, setQuery] = useState('react')
 
     useEffect(() => {
-        axios.get(BASE_URL + `query=${query}&page=${page - 1}`)
+        axios.get(BASE_URL + `query=${query}&page=${page}`)
             .then(({data}) => {
-                // console.log(data)
                 setPosts(data.hits)
                 setPageQty(data.nbPages)
+                console.log(data)
 
                 if (data.nbPages < page) {
                     setPage(1)
                 }
             })
+
+
     }, [page, query])
+
 
     return (
         <div>
             <TextField
                 value={query}
+                label={'Input'}
                 fullWidth
-                label={'TEST PAGINATION'}
                 onChange={event => setQuery(event.target.value)}
             />
             <Stack spacing={1}>
-                {posts.map((post, index) => (
-                    <Link key={index} href={post.url}>
-                        {post.title || post.story_title}
-                    </Link>
-                ))}
-
                 {!!pageQty && (
                     <Pagination
-                        className={'Pagination'}
                         count={pageQty}
-                        page={page}
                         showFirstButton
                         showLastButton
-                        onChange={(_, i) => setPage(i)}
-                        renderItem={
-                            (item) => (
-                                <PaginationItem
-                                    component={NavLink}
-                                    to={`/?page=${item.page}`}
-                                    {...item}
-                                />
-                            )
-                        }
-
+                        page={page}
+                        onChange={(_, index) => setPage(index)}
+                        renderItem={(item) => (
+                            <PaginationItem
+                                component={NavLink}
+                                to={`/?page=${item.page}`}
+                                {...item}
+                            />
+                        )}
                     />
                 )}
+
+                {
+                    posts.map((post, index) => (
+                        <Link key={index}>
+                            {post.title || post.story_title}
+                        </Link>
+                    ))
+                }
             </Stack>
         </div>
     );
